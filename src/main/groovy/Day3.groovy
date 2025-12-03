d1a0d0366cb9615b9bd38c9106b93ff8c5d37988
Ygimenez <yagogimenez1@hotmail.com>
2025-12-03 13:46:37 -0300

    Added support for day 3 part 2.

--- src/main/groovy/Day3.groovy
+++ src/main/groovy/Day3.groovy
@@ -1,9 +1,22 @@
 def input = getClass().getResource('/day3.txt').readLines()
 
+int digits = 12
 println input
 		.collect { it.split('') as List<String> }
 		*.with {
-			def head = it.dropRight(1).max()
-			return head + it.drop(it.indexOf(head) + 1).max() as int
+			def prio = it.toUnique().toSorted().reversed()
+			def queue = it
+
+			return (0..<digits)*.with {i ->
+				for (p in prio) {
+					int v = queue.dropRight(digits - i - 1).indexOf(p)
+					if (v > -1) {
+						queue = queue.drop(v + 1)
+						return p
+					}
+				}
+
+				throw "Reached end of input" as Throwable
+			}.join() as long
 		}
 		.sum()
