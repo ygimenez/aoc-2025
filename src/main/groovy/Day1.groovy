7d32a8b9d311690c5996e2b15e6584801a21e9b5
Ygimenez <yagogimenez1@hotmail.com>
2025-12-03 15:11:57 -0300

    Added part 2 for days 1 and 2.

--- src/main/groovy/Day1.groovy
+++ src/main/groovy/Day1.groovy
@@ -2,8 +2,20 @@ def input = getClass().getResource('/day1.txt').readLines()
 
 int zeros, dial = 50
 (input =~ /([LR])(\d+)/).each { _, dir, num ->
-	dial = (dial + (num as int) * (dir == 'R' ? 1 : -1) + 100) % 100
-	if (dial == 0) zeros++
+	int val = num as int
+	if (val > 99) {
+		zeros += val / 100 as int
+		val %= 100
+	}
+
+	int rot = dial + val * (dir == 'R' ? 1 : -1)
+	if (rot < 0) {
+		rot += 100
+		if (dial != 0) zeros++
+	}
+
+	dial = (rot + 100) % 100
+	zeros += dial ? (rot.abs() / 100 as int) : 1
 }
 
 println zeros
--- src/main/groovy/Day2.groovy
+++ src/main/groovy/Day2.groovy
@@ -2,5 +2,5 @@ def input = getClass().getResource('/day2.txt').text
 
 println input.split(',')*.split('-')
 		.collect { it*.toBigInteger() as NumberRange }
-		.flatten { it.findAll { it =~ /^(.+)\1$/ } }
+		.flatten { it.findAll { it =~ /^(.+)\1+$/ } }
 		.sum()
